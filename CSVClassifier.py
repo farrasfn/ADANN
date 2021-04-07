8a87cbc8e209b4ed23d9e749a3564abc30f954a0
farrasfn <farrasfnz@gmail.com>
2021-04-07 08:47:01 +0700

    ANN Model with 25 -> 10 -> 1 Layers, Acccuracy of 95%, model saved in /Models

--- CSVClassifier.py
+++ CSVClassifier.py
@@ -14,28 +14,45 @@ from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
 from keras.layers.normalization import BatchNormalization
 
 #read data w/ pandas
-df = pd.read_csv("Data/test.csv")
-print(df.head())
-print(df.dtypes)
-df['labels'] = pd.Categorical(df['labels'])
-df['labels'] = df.labels.cat.codes
-print(df.head())
+dftrain = pd.read_csv("Data/train.csv")
+print(dftrain.head())
+print(dftrain.dtypes)
+dftrain['labels'] = pd.Categorical(dftrain['labels'])
+dftrain['labels'] = dftrain.labels.cat.codes
+print(dftrain.head())
+
+ #test dataset
+dftest = pd.read_csv("Data/test.csv")
+print(dftest.head())
+print(dftest.dtypes)
+dftest['labels'] = pd.Categorical(dftest['labels'])
+dftest['labels'] = dftest.labels.cat.codes
+print(dftest.head())
 
 #load data using tf.data.Dataset
 
-target = df.pop('target')
+traintarget = dftrain.pop('target')
 
-dataset = tf.data.Dataset.from_tensor_slices((df.values, target.values))
+trdataset = tf.data.Dataset.from_tensor_slices((dftrain.values, traintarget.values))
 
-for feat, targ in dataset.take(5):
+for feat, targ in trdataset.take(5):
   print ('Features: {}, Target: {}'.format(feat, targ))
 
-  tf.constant(df['labels'])
-  train_dataset = dataset.shuffle(len(df)).batch(1)
+  tf.constant(dftrain['labels'])
+  train_dataset = trdataset.shuffle(len(dftrain)).batch(1)
 
+testtarget = dftest.pop('target')
+testdataset = tf.data.Dataset.from_tensor_slices((dftest.values, testtarget.values))
+for feat, targ in testdataset.take(5) :
+  print ('Features: {}, Target: {}'.format(feat, targ))
+
+  tf.constant(dftest['labels'])
+  test_dataset = testdataset.shuffle(len(dftest)).batch(1)
+
+#compiled model 
 def get_compiled_model():
   model = tf.keras.Sequential([
-    tf.keras.layers.Dense(10, activation='relu'),
+    tf.keras.layers.Dense(25, activation='relu'),
     tf.keras.layers.Dense(10, activation='relu'),
     tf.keras.layers.Dense(1)
   ])
@@ -45,5 +62,13 @@ def get_compiled_model():
                 metrics=['accuracy'])
   return model
 
+#fit the model
 model = get_compiled_model()
 model.fit(train_dataset, epochs=15)
+#save the model
+model.save('Models/FirstModel')
+#evaluate the model
+print ('EVALUATE')
+result = model.evaluate(test_dataset)
+dict(zip(model.metrics_names, result))
+
