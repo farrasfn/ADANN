e3fa8011dcc1b608e1c0271c1d0d46c25b26cbe9
farrasfn <farrasfnz@gmail.com>
2021-04-09 13:57:05 +0700

    Added Sending Email support

--- Interpreter.py
+++ Interpreter.py
@@ -28,4 +28,24 @@ cat=['Alzheimer = Positive','Alzheimer = Negative']
 output_data = interpreter.get_tensor(output_details[0]['index'])
 if (output_data >= 1):
     output_data = 1
-print(cat[int(output_data)])
+alzcond = (cat[int(output_data)])
+
+sname= 'Farras Nabil'
+snick= 'Farras'
+#SENDING EMAIL 
+import smtplib, ssl
+
+port = 465  # For SSL
+smtp_server = "smtp.gmail.com"
+sender_email = "farras.spyderboy@gmail.com"  # Enter your address
+receiver_email = "farrasfn@student.ub.ac.id"  # Enter receiver address
+password = "rl5t1ZIz2LZ11j8"
+message = """\
+Subject: Alzheimer Detection Result : {}
+
+Hello {},\n\nYour result from our Neural Network Diagnosis\n {} \nWith regards,\n\tADANN Admin""".format(sname,snick,alzcond)
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message)
